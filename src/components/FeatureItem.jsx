4e2af827dc9e131c1632c5e97c064461ae1ad8c7
Charles ASSOGBA <charlesclaudepascal@gmail.com>
2025-08-01 15:16:37 +0800

    creation of the footer and work on the FeatureItem

--- src/components/FeatureItem.jsx
+++ src/components/FeatureItem.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 import iconCart from '../assets/images/iconCart.png' 
+import image1 from '../assets/images/image1.png'
 const FeatureItem = () => {
   return (
     <div className='bg-white p-5 rounded-xl shadow-sm'>
         <Link to=''>
-          <img src='' alt="" className='w-full h-80 object-cover object-top drop-shadow-[0_80px_30px_#0007] '/>
+          <img src={image1} alt="" className='w-full h-90 object-cover object-top drop-shadow-[0_80px_30px_#0007] '/>
         </Link>
-        <h3 className='text-2xl py-3 text-center font-medium'>name</h3>
+        <h3 className='text-2xl py-3 text-center font-medium'>DELL</h3>
         <div className='flex justify-between items-center'>
             <p>
-                $<span className='text-2xl font-medium'>price</span>
+                $<span className='text-2xl font-medium'>8000</span>
             </p>
             <button className='bg-gray-300 p-2 rounded-md text-sm hover:bg-sky-500' onClick=''>
                 <img src={iconCart} className='w-5' alt=""/>
