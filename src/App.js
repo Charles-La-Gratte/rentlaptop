1a6ae3ac9cb8a8b6f3900c495958d57f6855bd90
Charles ASSOGBA <charlesclaudepascal@gmail.com>
2025-08-04 21:57:07 +0800

    work on the diplay of the featureItem

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Layout from './components/Layout';
+import Detail from './pages/Detail';
 import Home from './pages/Home';
 import { BrowserRouter,  Routes, Route } from 'react-router-dom';
 
@@ -9,6 +10,7 @@ function App() {
     <Routes>
       <Route path='/' element={<Layout/>}>
         <Route index element={<Home/>}/>
+        <Route path='/:slug' element={<Detail/>}/>
       </Route>
     </Routes>
     </BrowserRouter>
--- src/components/FeatureItem.jsx
+++ src/components/FeatureItem.jsx
@@ -1,19 +1,32 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom'
 import iconCart from '../assets/images/iconCart.png' 
-import image1 from '../assets/images/image1.png'
-const FeatureItem = () => {
+import { addToCart } from '../stores/cart'
+import { useDispatch, useSelector } from 'react-redux'
+
+const FeatureItem = (props) => {
+   const carts = useSelector(store => store.cart.items);
+   console.log(carts);
+   const {id, name, price, image, slug} = props.data;
+   const dispatch = useDispatch();
+   const handleAddToCart = () => {
+    dispatch(addToCart({
+      productId: id,
+      quantity: 1
+    }));
+   }
+
   return (
     <div className='bg-white p-5 rounded-xl shadow-sm'>
-        <Link to=''>
-          <img src={image1} alt="" className='w-full h-90 object-cover object-top drop-shadow-[0_80px_30px_#0007] '/>
+        <Link to={slug}>
+          <img src={image} alt="" className='w-full h-90 object-cover object-top drop-shadow-[0_80px_30px_#0007] '/>
         </Link>
-        <h3 className='text-2xl py-3 text-center font-medium'>DELL</h3>
+        <h3 className='text-2xl py-3 text-center font-medium'>{name}</h3>
         <div className='flex justify-between items-center'>
             <p>
-                $<span className='text-2xl font-medium'>8000</span>
+                $<span className='text-2xl font-medium'>{price}</span>
             </p>
-            <button className='bg-gray-300 p-2 rounded-md text-sm hover:bg-sky-500' onClick=''>
+            <button className='bg-gray-300 p-2 rounded-md text-sm hover:bg-sky-500' onClick={handleAddToCart}>
                 <img src={iconCart} className='w-5' alt=""/>
                 Add To Cart
             </button>
