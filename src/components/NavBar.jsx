7cbe0656c8bebc1b03f480c6d1a8dec374c34d24
Charles ASSOGBA <charlesclaudepascal@gmail.com>
2025-08-08 19:19:48 +0800

    fix the navBar responsivity

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -19,70 +19,18 @@ const NavBar = () => {
     dispatch(toggleStatusTab());
   }
   return (
-    <div>
-      <nav className="bg-white-800 p-4">
-        <div className="container mx-auto flex justify-between items-center">
-          <div className="text-blue-500 text-lg font-bold">RentLaptop</div>
-          <div className="block lg:hidden">
-            <button className="text-white focus:outline-none">
-              <svg
-                className="h-6 w-6"
-                fill="none"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                viewBox="0 0 24 24"
-                //strokeLinecap="currentColor"
-              >
-                <path d="M4 6h16M4 12h16m-7 6h7"></path>
-              </svg>
-            </button>
-          </div>
-          <div className="hidden lg:flex space-x-8">
-            <Link
-              to="/"
-              className="text-xl font-semibold text-black-300 hover:text-blue-600"
-            >
-              Home
-            </Link>
-            <Link
-              to="/rent"
-              className="text-xl font-semibold text-black-300 hover:text-blue-600"
-            >
-              Rent
-            </Link>
-            <Link
-              to="/contact"
-              className="text-xl font-semibold text-black-300 hover:text-blue-600"
-            >
-              Contact
-            </Link>
-            <Link
-              to="/contact"
-              className="text-xl font-semibold text-black-300 hover:text-blue-600"
-            >
-              Sign up
-            </Link>
-            <Link
-              to="/Login"
-              className="text-xl font-semibold text-black-300 hover:text-blue-600"
-            >
-              Login <FontAwesomeIcon icon={faSignIn} />
-            </Link>
-            <div
-              className="w-10 h-10 bg-gray-100 rounded-full flex justify-center items-center relative"
-              onClick={handleOpenTabCard}
-            >
-              <img src={iconCart} alt="" className="w-6" />
-              <span className="absolute top-2/3 right-1/2 bg-red-500 text-white text-sm w-5 h-5 rounded-full flex justify-center items-center">
-                {totalQuantity}
-              </span>
-            </div>
-          </div>
-        </div>
-      </nav>
-    </div>
-  );
-};
+    <header className='flex justify-between items-center mb-5'>
+      <div className="text-blue-500 text-lg font-bold">RentLaptop</div>
+       <Link to="/" className='text-xl font-semibold text-black-300 hover:text-blue-600'>Home</Link>
+       <Link to="/rent" className='text-xl font-semibold text-black-300 hover:text-blue-600'>Rent</Link>
+       <Link to="/signUp" className='text-xl font-semibold text-black-300 hover:text-blue-600'>Sign Up</Link>
+       <Link to="/login" className='text-xl font-semibold text-black-300 hover:text-blue-600'>Login</Link>
+       <div className='w-10 h-10 bg-gray-100 rounded-full flex justify-center items-center relative' onClick={handleOpenTabCard}>
+         <img src={iconCart} alt="" className='w-6' />
+         <span className='absolute top-2/3 right-1/2 bg-red-500 text-white text-sm w-5 h-5 rounded-full flex justify-center items-center'>{totalQuantity}</span>
+       </div>
+    </header>
+  )
+}
 
 export default NavBar;
